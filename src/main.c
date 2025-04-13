73cfb5fe03ab3218ca9bf7562d43c5a6ac71cadb
NDragos <dragosneagu28@gmail.com>
2025-04-13 23:42:21 +0300

    <Task 2>:<Am incercet simularea comenzii ls. In stiva am stocat directoarele, impreuna cu fisierele prezente in aceastea(o lista pentru fiecare element al stivei). Apoi am sortat stiva si fisierele. Am afisat rezulattul intr-un fisier de output.\>
    Signed-off-by: NDragos <dragosneagu28@gmail.com>

--- src/main.c
+++ src/main.c
@@ -1,101 +1,153 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "list.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/stat.h>
+#include<getopt.h>
+#include<dirent.h>
+#include "task2.h"
 
-void sort_arg(int argc, char *argv[], ARG **argument, ARG **option, ARG **flag) {
-    for (int i = 0; i < argc; i++) {
-        if(is_short(argv[i])) {
-            if(atoi(argv[i]) || (i + 1 < argc && atoi(argv[i + 1]))) {
-                opt(argv[i], argv[i + 1], option);
-                if(strlen((*option)->data.id) > 2) {
-			char *dup = strdup((*option)->data.id);
-			(*option)->data.id = dup;
-                    mult_arg((*option)->data.id, option);
-                    (*option)->data.id[2] = '\0';
-                }
+void util_ls(char* path, FILE *fout, Node **stack, int r, int d, int hr) {
 
-                if(atoi(argv[i + 1])) i++;
-            }
-            else {
-                add_flag(flag, argv[i]);
-                if(strlen((*flag)->data.id) > 2) {
-			char *dup = strdup((*flag)->data.id);
-			(*flag)->data.id = dup;
-                     mult_arg((*flag)->data.id, flag);
-                    (*flag)->data.id[2] = '\0';
-                }
-            }
+    Node *temp = NULL;
+    int fl = 0;
+
+    //fprintf(fout, "%d %d %d\n", r, d, hr);
+
+    //push(stack, path);
+    push(&temp, path);
+
+    while(temp != NULL) {
+
+        Data current_data = pop(&temp);
+        char *current_path = current_data.name;
+        DIR *dir = opendir(current_path);
+
+
+
+        if (dir == NULL)
+        {
+            fprintf(stderr, "Error opening directory %s (util_ls function)\n", path);
+            free(current_path);
+            exit(EXIT_FAILURE);
         }
-        else add_arg(argument, argv[i]);
+
+        struct dirent *entry;
+
+        while((entry = readdir(dir))) {
+
+                if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+                    if(fl == 0) {
+                         struct stat dirStat;
+                        if (stat(".", &dirStat) == 0) {
+                           if(hr)
+                                fprintf(fout ,"%.1fK %s\n", dirStat.st_size / 1024.0, ".");
+                           else
+                                fprintf(fout, "%ld %s\n", dirStat.st_size, ".");
+                            if(stat("..", &dirStat) == 0) {
+                                if(hr)
+                                    fprintf(fout ,"%.1fK %s\n", dirStat.st_size / 1024.0, "..");
+                                else
+                                    fprintf(fout, "%ld %s\n", dirStat.st_size, "..");
+                            }
+                        }
+                    }
+
+
+                    fl ++;
+                    continue;
+                }
+
+
+                char fullpath[PATH_MAX];
+                snprintf(fullpath, PATH_MAX, "%s/%s", current_path, entry->d_name);
+
+                struct stat fileStat;
+                if(lstat(fullpath, &fileStat) == 0) {
+
+                    if (S_ISLNK(fileStat.st_mode))
+                        continue;
+
+                    if(S_ISREG(fileStat.st_mode) && !d)
+                        {
+                            Node* aux = findir(*stack, current_path);
+                            if(aux) {
+                                //fprintf(fout, "%s\n", current_path);
+                                addbegin(&aux->content, entry->d_name, fileStat.st_size);
+                            }
+
+                        }else
+                            if(S_ISDIR(fileStat.st_mode))
+                            {
+                                push(stack, fullpath);
+                                push(&temp, fullpath);
+                            }
+                }
+                else
+                    fprintf(stderr,"lstat failed for %s\n", current_path);
+            }
+        closedir(dir);
+        free(current_path);
+        if(!d && !r)
+            break;
     }
 
+    sortdirs(stack);
+    sortfiles(&(*stack)->content);
+
+    printStack(*stack, fout, hr);
+    //printlist((*stack)->content, fout, hr);
+
+    freestack(temp);
 }
 
-int main(int argc, char **argv){
-    FILE *fin, *fout;
-    ARG *argument, *option, *flag;
-	argument = NULL;
-	option = NULL;
-	flag = NULL;
-    sort_arg(argc - 2, argv + 2, &argument, &option, &flag);
+int main(int argc, char **argv) {
+    Node* stack = NULL;
 
-    char infile[256] = {0}, outfile[256] = {0};
-        if (strncmp(argv[argc - 2], "--file=", 7) == 0) {
-		const char* posin = strstr(argv[argc - 2] + 7, "teste");
-	if(posin)
-		strcpy(infile, posin);
-	else
-            strcpy(infile, argv[argc - 2] + 7);
+    int recursive = 0, human_readable = 0, dir_only = 0;
+
+    int opt;
+    static struct option long_options[] = {
+        {"recursive", no_argument, 0, 'r'},
+        {"human-readable", no_argument, 0, 'h'},
+        {"directory", no_argument, 0, 'd'},
+        {"out", required_argument, 0, 'o'},
+        {0, 0, 0, 0}
+    };
+
+    char *out_path = NULL;
+
+    while((opt = getopt_long(argc, argv, "rhdo:", long_options, NULL)) != -1) {
+        switch(opt)
+        {
+
+            case 'r':
+                recursive = 1;
+            break;
+            case 'h':
+                human_readable = 1;
+            break;
+            case 'd':
+                dir_only = 1;
+            break;
+            case 'o':
+                out_path = optarg;
+                break;
+            default:
+                exit(EXIT_FAILURE);
+
         }
-	if (strncmp(argv[argc - 1], "--out=", 6) == 0) {
-		const char* posout = strstr(argv[argc - 1] + 6, "ref");
-	if(posout)
-		strcpy(outfile, posout);
-	else
-            strcpy(outfile, argv[argc - 1] + 6);
+    }
+    FILE *fout = fopen(out_path, "wt");
+
+        if(fout == NULL)
+        {
+            fprintf(stderr, "Error opening file\n");
+            exit(EXIT_FAILURE);
         }
 
-    fin = fopen(infile, "r");
-    if (!fin) {
-        fprintf(stderr, "Error opening file for reading: %s", infile);
-        exit(1);
-    }
+    util_ls(argv[optind + 1], fout, &stack, recursive, dir_only,human_readable);
 
-    fout = fopen(outfile, "w");
-    if (!fout) {
-        perror("Error opening file for writing");
-        fclose(fin);
-        exit(2);
-    }
-
-    char line[100];
-
-    while (fgets(line, sizeof(line), fin)) {
-
-        line[strcspn(line, "\n")] = '\0';
-
-        if (line[0] == 'a')
-            {
-            fprintf(fout, "%s ", line);
-            find_arg(&argument, fout);
-            }
-        else if (strstr(line, " f"))
-            {
-            fprintf(fout, "%s ", line);
-            find_f_otp(&flag, line, fout);
-            }
-        else if (strstr(line, " o"))
-            {
-            fprintf(fout, "%s ", line);
-            find_f_otp(&option, line, fout);
-            }
-    }
-
-    free_list(argument);
-    free_list(option);
-    free_list(flag);
-    fclose(fin);
+    freestack(stack);
     fclose(fout);
     return 0;
 }
